26a43f700aca3d58ff228405d757ec707ff800f7
Ludwig Muench <lu-i@hotmail.de>
2017-11-20 14:07:31 +0100

    updated index.js: commented out registerServiceWorker() to fix cache problem

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
