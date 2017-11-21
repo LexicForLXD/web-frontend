2698c000520c5a304207b6f95345dec347a44101
Ludwig Muench <lu-i@hotmail.de>
2017-11-21 14:07:23 +0100

    updated Login: increased client id to 11

--- src/Login.js
+++ src/Login.js
@@ -44,7 +44,7 @@ class Navigation extends Component {
       },
       body: JSON.stringify({
         grant_type: 'password',
-        client_id: '3_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4',
+        client_id: '11_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4',
         client_secret: '4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k',
         username: this.state.username,
         password: this.state.password
