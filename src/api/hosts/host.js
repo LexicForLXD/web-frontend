ac6ff73e8944e938e3fc83db1c4f0e67f957d116
Leon Lenzen <ich@leonlenzen.de>
2018-03-28 21:53:45 +0200

    feat: added host api with auth

--- src/api/hosts/host.js
+++ src/api/hosts/host.js
@@ -4,16 +4,19 @@ export default {
     fetch () {
         return client.withAuth().get('/hosts')
     },
-    page (url) {
-        return client.withAuth().get(url)
+    show (id) {
+        return client.withAuth().get('/hosts/'+id)
     },
     delete (id) {
         return client.withAuth().delete('/hosts/'+id)
     },
     create (host) {
-        return client.withAuth().post('/host', host)
+        return client.withAuth().post('/hosts', host)
     },
     update (id, host) {
-        return client.withAuth().put('/containers/'+id, host)
+        return client.withAuth().put('/hosts/'+id, host)
+    },
+    auth (id, body) {
+        return client.withAuth().post('/hosts/'+id+'/authorization', body)
     }
 }
