181beea606c98615c1a5fb0cdb3719dafff720a7
Leon Lenzen <ich@leonlenzen.de>
2018-04-01 22:27:04 +0200

    style: reformat

--- src/api/client.js
+++ src/api/client.js
@@ -1,41 +1,43 @@
-import { LEXIC_CONFIG } from '../config.js'
+import {LEXIC_CONFIG} from '../config.js'
 
 const client = {
-  auth: false,
-  withAuth() {
-    client.auth = true
-    return client
-  },
-  withoutAuth() {
-    client.auth = false
-    return client
-  }
+    auth: false,
+    withAuth() {
+        client.auth = true
+        return client
+    },
+    withoutAuth() {
+        client.auth = false
+        return client
+    }
 };
 
 ['get', 'post', 'put', 'delete'].forEach((verb) => {
 
-  client[verb] = (url, data = {}, headers = {}) => {
-    return new Promise((resolve, reject) => {
-      if(!url.startsWith("http")){
-        url = LEXIC_CONFIG.API_URL + url
-      }
-      return window.axios({
-        method: verb,
-        url,
-        data,
-        headers: client.auth ? Object.assign({}, { 'Authorization': `Bearer ` + localStorage.getItem('access_token') }, headers) : headers
-      }).then((res) => {
-        client.auth = false
-        resolve(res)
-      }).catch((error) => {
-        client.auth = false
-        reject(error)
-      })
-    })
-  }
+    client[verb] = (url, data = {}, headers = {}) => {
+        return new Promise((resolve, reject) => {
+            if (!url.startsWith("http")) {
+                url = LEXIC_CONFIG.API_URL + url
+            }
+            return window.axios({
+                method: verb,
+                url,
+                data,
+                headers: client.auth ? Object.assign({}, {'Authorization': `Bearer ` + localStorage.getItem('access_token')}, headers) : headers
+            }).then((res) => {
+                client.auth = false
+                resolve(res)
+            }).catch((error) => {
+                client.auth = false
+                reject(error)
+            })
+        })
+    }
 })
 
+
 export default client;
+
 function newFunction() {
-  return 'get';
+    return 'get';
 }
