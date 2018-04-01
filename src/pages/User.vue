5d8befdbf7824a770b31de9956a688aeddab5018
Leon Lenzen <ich@leonlenzen.de>
2018-04-01 22:30:16 +0200

    feat: added user page

---
+++ src/pages/User.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+        <div v-if="this.$store.state.route.path != '/users/new'">
+            <button @click="refresh" class="button"><i class="fa fa-retweet"></i></button>
+        </div>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            refresh() {
+                this.$store.dispatch("getUsers");
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
