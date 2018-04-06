e1d9691febea6bef45b91190bca28ab38b731b24
Leon Lenzen <ich@leonlenzen.de>
2018-04-06 14:23:22 +0200

    refactor: split container module in files

---
+++ src/store/modules/containers/actions.js
@@ -0,0 +1,79 @@
+import containerApi from "../../../api/containers/container";
+import * as types from "../../mutation-types";
+
+export default {
+    setContainers({commit}) {
+        commit(types.LOADING_BEGIN);
+        commit(types.CONTAINER_LOADING_START);
+        containerApi.fetch().then((res) => {
+            commit(types.CONTAINER_SET_ALL, {containersData: res.data});
+            commit(types.CONTAINER_LOADING_SUCCESS);
+            commit(types.LOADING_FINISH);
+        }).catch((error) => {
+            commit(types.CONTAINER_SET_ALL_FAILURE, error);
+            commit(types.LOADING_FAIL);
+        })
+    },
+
+    initContainers({commit}) {
+        return new Promise((resolve, reject) => {
+            containerApi.fetch().then((res) => {
+                commit(types.CONTAINER_SET_ALL, {containersData: res.data});
+                resolve();
+            }).catch((error) => {
+                if (error.response) {
+                    if (error.response.status === 404) {
+                        if (error.response.data.error.code === 404) {
+                            resolve(error.response.data.error.message);
+                        }
+                    }
+                }
+                reject(error);
+            })
+        })
+    },
+
+
+    deleteContainer({commit}, id) {
+        commit(types.CONTAINER_DELETE, id);
+        commit(types.LOADING_BEGIN);
+        containerApi.delete(id).then(() => {
+            commit(types.CONTAINER_DELETE_SUCCESS);
+            commit(types.LOADING_FINISH);
+        }).catch((error) => {
+            commit(types.CONTAINER_DELETE_FAILURE, error);
+            commit(types.LOADING_FAIL);
+        })
+    }
+    ,
+
+
+    createContainer({commit}, data) {
+        commit(types.LOADING_BEGIN);
+        return new Promise((resolve, reject) => {
+            containerApi.create(data.hostId, data.container, data.type).then((res) => {
+                commit(types.CONTAINER_ADD_NEW, {container: res.data});
+                commit(types.LOADING_FINISH);
+                commit(types.CONTAINER_ADD_NEW_SUCCESS);
+                resolve();
+            }).catch((error) => {
+                commit(types.CONTAINER_ADD_NEW_FAILURE, error);
+                commit(types.LOADING_FAIL);
+                reject();
+            })
+        })
+    }
+    ,
+
+
+    updateContainer({commit}, data) {
+        commit(types.LOADING_BEGIN);
+        containerApi.update(data.hostId, data).then((res) => {
+            commit(types.CONTAINER_UPDATE_SUCCESS, {containers: res.data});
+            commit(types.LOADING_FINISH);
+        }).catch((error) => {
+            commit(types.LOADING_FAIL);
+            commit(types.CONTAINER_UPDATE_FAILURE, error);
+        })
+    }
+}
---
+++ src/store/modules/containers/mutations.js
@@ -0,0 +1,126 @@
+import * as types from "../../mutation-types";
+import {forEach, pull} from "lodash";
+import {keyForContainer} from "./index";
+
+export default {
+    [types.CONTAINER_DELETE]({containers, containersList, deletedContainer}, id) {
+        const key = keyForContainer(id);
+        deletedContainer = containers[key];
+        Vue.delete(containers, key);
+        pull(containersList, id);
+    },
+
+    [types.CONTAINER_DELETE_FAILURE](state, error) {
+        const key = keyForContainer(state.deletedContainer.id);
+        state.containersList.push(state.deletedContainer.id);
+        Vue.set(state.containers, key, state.deletedContainer);
+        state.deletedContainer = {};
+        setErrors(state.containerErrors, error)
+    },
+
+    [types.CONTAINER_DELETE_SUCCESS](state) {
+        state.deletedContainer = {};
+        clearErrors(state.containerErrors);
+    },
+
+
+    [types.CONTAINER_SET_ALL]({containers, containersList, containerErrors}, {containersData}) {
+        forEach(containersData, function (value) {
+            const key = keyForContainer(value.id);
+            if (!containers[key]) {
+                containersList.push(value.id)
+            }
+            Vue.set(containers, key, value);
+            clearErrors(containerErrors);
+        })
+    },
+
+    [types.CONTAINER_SET_ALL_FAILURE] ({containerErrors}, error) {
+        setErrors(containerErrors, error);
+    },
+
+    [types.CONTAINER_UPDATE_SUCCESS](state, container) {
+        const key = keyForContainer(container.id);
+
+        state.containers[key] = container;
+        clearErrors(state.containerErrors);
+    },
+
+    [types.CONTAINER_UPDATE_FAILURE]({containerErrors}, error) {
+        setErrors(containerErrors, error);
+    },
+
+    [types.CONTAINER_ADD_NEW]({containers, containersList}, {container}) {
+        const key = keyForContainer(container.id);
+        if (!containers[key]) {
+            Vue.set(containers, key, container);
+            containersList.push(container.id)
+        }
+    },
+
+    [types.CONTAINER_ADD_NEW_SUCCESS]({containerErrors}) {
+        clearErrors(containerErrors);
+    },
+
+    [types.CONTAINER_ADD_NEW_FAILURE]({containerErrors}, error) {
+        setErrors(containerErrors, error);
+    }
+
+}
+
+
+
+function setErrors(containerErrors, error) {
+    if (error.response.data.error.message.name) {
+        containerErrors.name = error.response.data.error.message.name;
+    } else {
+        containerErrors.name = "";
+    }
+    if (error.response.data.error.message.ipv4) {
+        containerErrors.ipv4 = error.response.data.error.message.ipv4;
+    } else {
+        containerErrors.ipv4 = "";
+    }
+    if (error.response.data.error.message.ipv6) {
+        containerErrors.ipv6 = error.response.data.error.message.ipv6;
+    } else {
+        containerErrors.ipv6 = "";
+    }
+    if (error.response.data.error.message.domainName) {
+        containerErrors.domainName = error.response.data.error.message.domainName;
+    } else {
+        containerErrors.domainName = "";
+    }
+    if (error.response.data.error.message.architecture) {
+        containerErrors.architecture = error.response.data.error.message.architecture;
+    } else {
+        containerErrors.architecture = "";
+    }
+    if (error.response.data.error.message.config) {
+        containerErrors.config = error.response.data.error.message.config;
+    } else {
+        containerErrors.config = "";
+    }
+    if (error.response.data.error.message.devices) {
+        containerErrors.devices = error.response.data.error.message.devices;
+    } else {
+        containerErrors.devices = "";
+    }
+    if (error.response.data.error.message.sourceType) {
+        containerErrors.sourceType = error.response.data.error.message.sourceType;
+    } else {
+        containerErrors.sourceType = "";
+    }
+}
+
+
+function clearErrors(containerErrors) {
+    containerErrors.name = "";
+    containerErrors.ipv4 = "";
+    containerErrors.ipv6 = "";
+    containerErrors.domainName = "";
+    containerErrors.architecture = "";
+    containerErrors.config = "";
+    containerErrors.devices = "";
+    containerErrors.sourceType = "";
+}
