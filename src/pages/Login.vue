b4d3d05c9be62de8a437e43a9982eca0ebc65f02
Leon Lenzen <ich@leonlenzen.de>
2018-04-06 14:28:57 +0200

    fix: reload on login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,43 +1,60 @@
 <template>
-    <div class="loginform">
-        <div class="card">
-            <header class="card-header">
-                <div class="card-header-title">
-                    <h3> Login </h3>
-                </div>
-            </header>
-            <div class="card-content">
-                <div class="field">
-                    <label class="label">Username</label>
-                    <div class="control">
-                        <input class="input" type="text" v-model="email">
-                    </div>
-                </div>
-
-                <div class="field">
-                    <label class="label">Password</label>
-                    <div class="control">
-                        <input class="input" type="password" v-model="password">
-                    </div>
-                </div>
-
-                <div v-if="error.length > 0" class="message is-danger">
-                    <div class="message-body">
-                        {{error}}
-                    </div>
-                </div>
-
-                <div>
-                    <button class="button is-success" @click="login">Login</button>
-                </div>
-
-            </div>
-
-
-            <!--<router-link to="/forget-password">Password vergessen?</router-link>-->
-
+  <div class="loginform">
+    <div class="card">
+      <header class="card-header">
+        <div class="card-header-title">
+          <h3> Login </h3>
         </div>
+      </header>
+      <div class="card-content">
+        <form 
+          @submit="login"
+          @submit.prevent>
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                v-model="email"
+                required>
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input
+                class="input"
+                type="password"
+                v-model="password"
+                required>
+            </div>
+          </div>
+
+          <div
+            v-if="error.length > 0"
+            class="message is-danger">
+            <div class="message-body">
+              {{ error }}
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="button is-success"
+              @click="login">Login</button>
+          </div>
+        </form>
+
+      </div>
+
+
+      <!--<router-link to="/forget-password">Password vergessen?</router-link>-->
+
     </div>
+  </div>
 </template>
 
 <script>
@@ -62,7 +79,7 @@
                 this.$store.commit("LOADING_BEGIN");
                 authApi.login(data)
                     .then(res => {
-                        this.error= "";
+                        this.error = "";
                         const expiration = (res.data.expires_in * 1000) + Date.now();
                         localStorage.setItem('access_token', res.data.access_token);
                         localStorage.setItem('expiration', expiration);
