076275d32410f3a4685d9e94ba0947df15278f9b
Leon Lenzen <ich@leonlenzen.de>
2018-04-07 16:27:04 +0200

    refactor: added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
     <div id="app">
-        <div v-if="isInitiated || !isAuthenticated" class="container">
-            <vue-progress-bar></vue-progress-bar>
-            <site-header class="site-header"></site-header>
-            <site-nav class="sidebar"></site-nav>
-            <router-view class="content"></router-view>
-            <site-footer class="site-footer"></site-footer>
-        </div>
-        <div v-else>
-            <site-loading/>
-        </div>
+        <v-app>
+            <site-nav/>
+            <site-header/>
+            <v-content>
+                <v-container fluid>
+                    <router-view v-if="isInitiated || !isAuthenticated"></router-view>
+                    <div v-else>
+                        <site-loading/>
+                    </div>
+                </v-container>
+            </v-content>
+            <v-footer app>
+                <site-footer/>
+            </v-footer>
+        </v-app>
     </div>
 </template>
 
@@ -85,32 +90,32 @@
 </script>
 
 <style lang="scss">
-    @import "~bulma/sass/utilities/_all";
-    @import "~bulma/sass/elements/button";
-    @import "~bulma/sass/elements/table";
-    @import "~bulma/sass/elements/form";
-    @import "~bulma/sass/components/navbar";
-    // @import "~bulma/sass/components/modal";
-    @import "~bulma/sass/components/menu";
-    @import "~bulma/sass/components/pagination";
-    @import "~bulma/sass/components/panel";
-    @import "~bulma/sass/components/card";
-    @import "~bulma/sass/components/message";
+    /*@import "~bulma/sass/utilities/_all";*/
+    /*@import "~bulma/sass/elements/button";*/
+    /*@import "~bulma/sass/elements/table";*/
+    /*@import "~bulma/sass/elements/form";*/
+    /*@import "~bulma/sass/components/navbar";*/
+    /*// @import "~bulma/sass/components/modal";*/
+    /*@import "~bulma/sass/components/menu";*/
+    /*@import "~bulma/sass/components/pagination";*/
+    /*@import "~bulma/sass/components/panel";*/
+    /*@import "~bulma/sass/components/card";*/
+    /*@import "~bulma/sass/components/message";*/
 
-    .container {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        grid-template-rows: auto;
-        grid-template-areas: "sidebar header header header" "sidebar content content content" "sidebar footer footer footer";
+    /*.container {*/
+    /*display: grid;*/
+    /*grid-template-columns: 1fr 1fr 1fr 1fr;*/
+    /*grid-template-rows: auto;*/
+    /*grid-template-areas: "sidebar header header header" "sidebar content content content" "sidebar footer footer footer";*/
 
-    }
+    /*}*/
 
-    .content {
-        grid-area: content;
-        padding: 10px 10px 10px 10px;
-    }
+    /*.content {*/
+    /*grid-area: content;*/
+    /*padding: 10px 10px 10px 10px;*/
+    /*}*/
 
-    body {
-        margin: 0;
-    }
+    /*body {*/
+    /*margin: 0;*/
+    /*}*/
 </style>
