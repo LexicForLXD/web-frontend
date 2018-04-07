3173b3b15a445b2d3ebbd307d0c1395d60bd560c
Leon Lenzen <ich@leonlenzen.de>
2018-04-07 16:33:55 +0200

    refactor: removed obsolet menues

--- src/components/sidebar/Menu.vue
+++ src/components/sidebar/Menu.vue
@@ -72,77 +72,15 @@
                 </v-list-tile>
             </v-list-group>
         </v-list>
-        <!--<v-list class="pt-0">-->
-        <!--<v-list-tile>-->
-        <!--<v-list-tile-title class="title">-->
-        <!--General-->
-        <!--</v-list-tile-title>-->
-        <!--</v-list-tile>-->
-        <!--<v-divider></v-divider>-->
-        <!--<host-menu></host-menu>-->
-        <!--<container-menu></container-menu>-->
-        <!--<profile-menu></profile-menu>-->
-        <!--<image-menu></image-menu>-->
-        <!--<v-list-tile>-->
-        <!--<v-list-tile-title class="title">-->
-        <!--Backup-->
-        <!--</v-list-tile-title>-->
-        <!--</v-list-tile>-->
-        <!--<v-divider></v-divider>-->
-        <!--<backup-menu></backup-menu>-->
-        <!--<schedule-menu></schedule-menu>-->
-        <!--<dest-menu></dest-menu>-->
-        <!--<v-list-tile>-->
-        <!--<v-list-tile-title class="title">-->
-        <!--Backup-->
-        <!--</v-list-tile-title>-->
-        <!--</v-list-tile>-->
-        <!--<v-divider></v-divider>-->
-        <!--<user-menu></user-menu>&ndash;&gt;-->
-        <!--</v-list>-->
+
     </v-navigation-drawer>
-    <!--<aside class="menu">-->
-    <!--<p class="menu-label">General</p>-->
-    <!--<ul class="menu-list">-->
-    <!--<host-menu></host-menu>-->
-    <!--<container-menu></container-menu>-->
-    <!--<profile-menu></profile-menu>-->
-    <!--<image-menu></image-menu>-->
-    <!--</ul>-->
-    <!--<p class="menu-label">Backup</p>-->
-    <!--<ul class="menu-list">-->
-    <!--<backup-menu></backup-menu>-->
-    <!--<schedule-menu></schedule-menu>-->
-    <!--<dest-menu></dest-menu>-->
-    <!--</ul>-->
-    <!--<p class="menu-label">Sonstiges</p>-->
-    <!--<ul class="menu-list">-->
-    <!--<user-menu></user-menu>-->
-    <!--</ul>-->
-    <!--</aside>-->
+
 </template>
 
 <script>
-    import hostMenu from './HostMenu'
-    import containerMenu from './ContainerMenu'
-    import scheduleMenu from './ScheduleMenu'
-    import destMenu from './DestinationMenu'
-    import profileMenu from './ProfileMenu'
-    import imageMenu from './ImageMenu'
-    import backupMenu from './BackupMenu'
-    import userMenu from './UserMenu'
+
 
     export default {
-        components: {
-            hostMenu,
-            containerMenu,
-            scheduleMenu,
-            destMenu,
-            profileMenu,
-            imageMenu,
-            backupMenu,
-            userMenu
-        },
 
         data() {
             return {
