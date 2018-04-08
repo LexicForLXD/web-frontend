fc9c10da75f7500fa60412bfa2e68cca1e8ba4bb
Leon Lenzen <ich@leonlenzen.de>
2018-04-08 19:41:34 +0200

    refactor: backup destination new and list views

--- src/components/backup-destination/BackupDestinationList.vue
+++ src/components/backup-destination/BackupDestinationList.vue
@@ -18,42 +18,11 @@
                 </td>
             </template>
         </v-data-table>
-
-
-        <!--<div>-->
-        <!--<div v-if="backupDests.length > 0">-->
-        <!--<table class="table is-hoverable is-fullwidth">-->
-        <!--<thead>-->
-        <!--<tr>-->
-        <!--<th>Name</th>-->
-        <!--<th>Protocol</th>-->
-        <!--<th>Path</th>-->
-        <!--</tr>-->
-        <!--</thead>-->
-        <!--<tbody>-->
-        <!--<router-link v-for="(backupDest, index) in backupDests"-->
-        <!--:key="backupDest.id"-->
-        <!--:to="{name: 'destinationSingle', params: {index: index}}"-->
-        <!--tag="tr">-->
-        <!--<td>{{backupDest.name}}</td>-->
-        <!--<td>{{backupDest.protocol}}</td>-->
-        <!--<td>{{backupDest.path}}</td>-->
-        <!--</router-link>-->
-        <!--</tbody>-->
-        <!--</table>-->
-        <!--</div>-->
-
-
-        <!--<div v-else>-->
-        <!--Keine Einträge vorhanden-->
-        <!--</div>-->
-
     </div>
 </template>
 
 <script>
     import {mapGetters} from "vuex";
-    // import Workout from "./Workout";
 
     export default {
         data() {
