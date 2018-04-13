22995848fa216b4b4aefb612a75f27599136cd26
Leon Lenzen <ich@leonlenzen.de>
2018-04-13 11:06:20 +0200

    refactor: updated backup restore api and added comments

--- src/api/backup/backupRestore.js
+++ src/api/backup/backupRestore.js
@@ -1,9 +1,23 @@
 import client from '../client'
 
 export default {
-    getFiles(backupId) {
-        return client.withAuth().get('/restores/backups/'+backupId)
+    /**
+     * Getting the array of the tarballs on the selected host
+     *
+     * @param backupId
+     * @param hostId
+     * @returns AxiosPromise
+     */
+    getFiles(backupId, hostId) {
+        return client.withAuth().get('/restores/backups/'+backupId+'?host='+hostId)
     },
+
+    /**
+     * Restore a selected backup
+     *
+     * @param backupId
+     * @returns {AxiosPromise<any>}
+     */
     restore(backupId) {
         return client.withAuth().post('/restores/backups/' + backupId)
     }
