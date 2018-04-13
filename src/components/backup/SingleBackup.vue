acc28ec5ef87965d01f7ae4ed1cbe288cb8b5e33
Leon Lenzen <ich@leonlenzen.de>
2018-04-13 11:09:54 +0200

    refactor: single backup view with little restore

--- src/components/backup/SingleBackup.vue
+++ src/components/backup/SingleBackup.vue
@@ -1,26 +1,69 @@
 <template>
-    <div>
-        {{backup.timestamp}}
-        <p>{{destination.name}}</p>
-        <p>{{schedule.name}}</p>
-    </div>
+    <v-card v-if="backup">
+        <v-toolbar>
+            <v-toolbar-title>
+                {{backup.timestamp}}
+            </v-toolbar-title>
+        </v-toolbar>
+        <v-card-text v-if="!restore">
+            <p><b>Destination:</b> {{destination.name}}</p>
+            <p><b>Schedule:</b> {{schedule.name}}</p>
+            <p><b>Containers:</b></p>
+            <ul v-for="container in containers">
+                <li> {{container.name}}</li>
+            </ul>
+
+        </v-card-text>
+        <v-card-text v-else>
+            Select a host to get a list of possible tarballs
+            <v-select
+                    :items="hosts"
+                    v-model="hostId"
+                    label="Host"
+                    required
+                    item-value="id"
+                    item-text="name"
+                    :rules="[v => !!v || 'Host is required']"
+            />
+            <v-list>
+                <v-list-tile v-for="tarball in tarballList" :key="tarball.name">
+                    <v-list-tile-action>
+                        <v-icon>restore</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+
+            </v-list>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn @click="restore = true" v-show="!restore">Delete</v-btn>
+            <v-btn @click="restore = false" v-show="restore">Abort</v-btn>
+            <v-btn @click="onStartRestore">Restore</v-btn>
+        </v-card-actions>
+    </v-card>
 
 </template>
 
 <script>
-    import {mapGetters} from "vuex";
+    import {mapGetters, mapMutations} from "vuex";
+    import restoreApi from "../../api/backup/backupRestore"
+    import {LOADING_BEGIN, LOADING_FAIL, LOADING_FINISH} from "../../store/mutation-types";
+
 
     export default {
         computed: {
-            backup () {
+            ...mapGetters({
+                hosts: "getHosts"
+            }),
+
+            backup() {
                 return this.$store.getters.getBackupByIndex(this.index);
             },
 
-            destination () {
+            destination() {
                 return this.$store.getters.getBackupDestinationById(this.backup.destinationId);
             },
 
-            destinationIndex () {
+            destinationIndex() {
                 return this.$store.getters.getBackupDestinationIndexById(this.backup.destinationId);
             },
 
@@ -38,54 +81,71 @@
         },
         data() {
             return {
-                editing: false,
-                editIpv4: "",
-                editIpv6: "",
-                editDomainName: "",
-                editName: "",
-                editPort: "",
-                // editSettings: "",
-                // editMac: "",
+                restore: false,
+                error: "",
                 index: this.$route.params.index,
+                tarballList: [],
+                hostId: "",
             };
         },
+
+        watch: {
+            hostId: function () {
+                this.getTarballs();
+            }
+        },
+
+
         methods: {
+            ...
+                mapMutations({
+                    startLoading: LOADING_BEGIN,
+                    stopLoading: LOADING_FINISH,
+                    failLoading: LOADING_FAIL
+                }),
 
             containerIndex(id) {
                 return this.$store.getters.getContainerIndexById(id);
             },
 
             onDelete() {
-                this.$store.dispatch("deleteHost", this.id);
+                this.$store.dispatch("deleteBackup", this.backup.id);
             },
-            onEdit() {
-                this.editIpv4 = this.hosts[this.index].ipv4;
-                this.editIpv6 = this.hosts[this.index].ipv6;
-                this.editDomainName = this.hosts[this.index].domainName;
-                this.editName = this.hosts[this.index].name;
-                this.editPort = this.hosts[this.index].port;
-                this.editing = true;
-            },
+
             onCancel() {
-                this.editing = false;
+                this.restore = false;
             },
-            onUpdate() {
-                this.$store.dispatch("updateHost", {
-                    host_id: this.hosts[this.index].id,
-                    host: {
-                        name: this.editName,
-                        ipv4: this.editIpv4,
-                        ipv6: this.editIpv6,
-                        domainName: this.editDomainName,
-                        port: this.editPort,
-                    }
-                });
-                this.editing = false;
+
+            onStartRestore() {
+                this.restore = true;
             },
+
+            getTarballs() {
+                this.startLoading();
+                restoreApi.getFiles(this.backup.id, this.hostId).then(res => {
+                    this.tarballList = res.data;
+                    this.stopLoading();
+                }).catch(error => {
+                    this.error = error;
+                    this.failLoading();
+                })
+            },
+
+            onRestore() {
+                this.startLoading();
+                restoreApi.restore(this.backup.id).then(res => {
+                    this.logsList = res.data;
+                    this.stopLoading();
+                }).catch(error => {
+                    this.error = error;
+                    this.failLoading();
+                })
+            }
 
 
         }
-    };
+    }
+    ;
 </script>
 
 <style>
