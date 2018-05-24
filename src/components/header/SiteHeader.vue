6ce7dd0bb988e2fa1910c3623534e2b0b52aecee
Leon Lenzen <ich@leonlenzen.de>
2018-05-25 00:11:59 +0200

    refactor: removed code from header

--- src/components/header/SiteHeader.vue
+++ src/components/header/SiteHeader.vue
@@ -6,19 +6,6 @@
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <user-badge></user-badge>
-        <!--<nav class="navbar">-->
-            <!--<div class="navbar-brand">-->
-                <!--<a class="navbar-item">LEXIC</a>-->
-                <!--<a class="navbar-burger" @click="mobileMenu" v-bind:class="{'is-active': menuExpanded}">-->
-                    <!--<span></span>-->
-                    <!--<span></span>-->
-                    <!--<span></span>-->
-                <!--</a>-->
-            <!--</div>-->
-            <!--<div class="navbar-menu" v-bind:class="{'is-active': menuExpanded}">-->
-                <!--<user-badge></user-badge>-->
-            <!--</div>-->
-        <!--</nav>-->
     </v-toolbar>
 </template>
 
@@ -47,10 +34,6 @@
 </script>
 
 <style>
-    .site-header {
-        grid-area: header;
-        background: #FBB731;
-        padding: 10px;
-    }
+
 
 </style>
