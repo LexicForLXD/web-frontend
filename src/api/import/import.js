3479109799a80d7ef7cde35a91e4e22100309316
Leon Lenzen <ich@leonlenzen.de>
2018-08-29 23:07:27 +0200

    fix: missing import api

--- src/api/import/import.js
+++ src/api/import/import.js
@@ -1,41 +1,48 @@
-import client from '../client'
+import client from "../client";
 
 export default {
+  /**
+   * Import images from host.
+   * @param hostId
+   * @returns {AxiosPromise<any>}
+   */
+  images(hostId) {
+    return client.withAuth().post("/sync/hosts/" + hostId + "/images");
+  },
 
-    /**
-     * Import images from host.
-     * @param hostId
-     * @returns {AxiosPromise<any>}
-     */
-    images(hostId) {
-        return client.withAuth().post('/sync/hosts/' + hostId + '/images')
-    },
+  /**
+   * Import profiles from host.
+   * @param hostId
+   * @returns {AxiosPromise<any>}
+   */
+  profiles(hostId) {
+    return client.withAuth().post("/sync/hosts/" + hostId + "/profiles");
+  },
 
-    /**
-     * Import containers from host.
-     * @param hostId
-     * @returns {AxiosPromise<any>}
-     */
-    containers(hostId) {
-        return client.withAuth().post('/sync/hosts/' + hostId + '/containers')
-    },
+  /**
+   * Import containers from host.
+   * @param hostId
+   * @returns {AxiosPromise<any>}
+   */
+  containers(hostId) {
+    return client.withAuth().post("/sync/hosts/" + hostId + "/containers");
+  },
 
-    /**
-     * Import storage pools from host.
-     * @param hostId
-     * @returns {AxiosPromise<any>}
-     */
-    storagePools(hostId) {
-        return client.withAuth().post('/sync/hosts/' + hostId + '/storage-pools')
-    },
+  /**
+   * Import storage pools from host.
+   * @param hostId
+   * @returns {AxiosPromise<any>}
+   */
+  storagePools(hostId) {
+    return client.withAuth().post("/sync/hosts/" + hostId + "/storage-pools");
+  },
 
-    /**
-     * Import images and containers at the same time.
-     * @param hostId
-     * @returns {AxiosPromise<any>}
-     */
-    all(hostId) {
-        return client.withAuth().post('/sync/hosts/' + hostId)
-    }
-
-}
+  /**
+   * Import images and containers at the same time.
+   * @param hostId
+   * @returns {AxiosPromise<any>}
+   */
+  all(hostId) {
+    return client.withAuth().post("/sync/hosts/" + hostId);
+  }
+};
