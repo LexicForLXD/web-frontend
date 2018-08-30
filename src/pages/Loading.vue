a2f17e9d58a5c93f60b36a4b9d7378094abf5008
Leon Lenzen <ich@leonlenzen.de>
2018-08-30 10:26:28 +0200

    refactor: loading animation

--- src/pages/Loading.vue
+++ src/pages/Loading.vue
@@ -1,28 +1,19 @@
 <template>
-    <div>
+    <div class="text-xs-center">
 
         <h1>Waiting for initialization.</h1>
 
-
-        <img
-                src="assets/images/logo.png"
-                alt="Logo">
-
-        <h3>Loading screen</h3>
-
+        <v-progress-circular :size="150" :width="10" indeterminate />
 
         <h2>{{error}}</h2>
     </div>
 </template>
 
 <script>
-    export default {
-        props: [
-            'error'
-        ]
-    }
+export default {
+  props: ["error"]
+};
 </script>
 
 <style>
-
 </style>
